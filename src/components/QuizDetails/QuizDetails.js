6ad64feea73bf279298c98e09df7365c4217b13e
shakil <mdshakil2898@gmail.com>
2022-10-10 15:36:57 -0700

    toast added

--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import { EyeIcon } from '@heroicons/react/24/solid'
+import {toast, ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 const QuizDetails = ({quiz, idx}) => {
     console.log(quiz)
@@ -7,6 +11,19 @@ const QuizDetails = ({quiz, idx}) => {
     const showAnswer = () =>{
         setAnswer(true);
     }
+    const demo = (event) =>{
+        console.log(event.target.value)
+        const choosenAnswer = event.target.value;
+        if(choosenAnswer === correctAnswer){
+            toast('Correct answer');
+        }
+        else{
+            toast('your answer is wrong');
+        }
+    }
+
+   
+    
     const {options, question, correctAnswer } = quiz;
     return (
         <div className='bg-orange-300 m-6 p-6 rounded-md'>
@@ -15,22 +32,25 @@ const QuizDetails = ({quiz, idx}) => {
             <p>{question}</p>
             <EyeIcon onClick={showAnswer} className="h-6 w-6  text-blue-500"/>
             </div>
+            {/* <div>
+                <button onClick={demo}>click me</button>
+            </div> */}
             
-            <div className='bg-red-300'>
+            <div className='bg-red-500 my-3 rounded-md'>
                 {
-                    answer && <h4>Correct Answer :{correctAnswer}</h4>
+                    answer && <h4>Correct Answer : {correctAnswer}</h4>
                 }
             </div>
 
-            <div className='text-start'>
+            <div className='text-start ' onClick={demo}>
                 {
-                    options.map(option => <div>
-                        <input type='radio' name='javascript'></input>
-               <span className='ml-2'>{option}</span><br></br>
+                    options.map((option, idx) => <div>
+                        <input type='radio'  name='javascript' value={option}></input>
+               <label  className='ml-2 hover:bg-amber-700 w-full rounded'>{option}</label><br></br>
                     </div>)
                 }
             </div>
-            
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
