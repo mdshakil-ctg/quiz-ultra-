e109085b93ab90e65ea55e7ed1dc3da029810de9
shakil <mdshakil2898@gmail.com>
2022-10-11 03:36:35 -0700

    recharts added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home/Home';
 import CartDetails from './components/CartDetails/CartDetails';
 import ErrorPage from './components/ErrorPage/ErrorPage';
 import Blogs from './components/Blogs/Blogs';
+import Statistics from './components/Statistics/Statistics';
 
 function App() {
   const router = createBrowserRouter([
@@ -22,6 +23,11 @@ function App() {
           path:'/quiz/:cartId',
           loader: async({params}) => fetch(`https://openapi.programming-hero.com/api/quiz/${params.cartId}`),
           element: <CartDetails></CartDetails>
+        },
+        {
+          path:'/statistics',
+          loader: async()=> fetch('https://openapi.programming-hero.com/api/quiz'),
+          element:<Statistics></Statistics>
         },
         {
           path:'/blogs',
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import {
+
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend
+  } from "recharts";
+
+  
+
+const Charts = () => {
+    const quizData = useLoaderData();
+    const data = quizData.data;
+    return (
+       <div className='bg-yellow-300 p-11'>
+            <LineChart width={500}  height={300} data={data}>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip></Tooltip>
+            <Line type="monotone" dataKey="total" stroke="#8884d8" activeDot={{ r: 8 }} />
+            </LineChart>
+       </div>
+    );
+};
+
+export default Charts;
